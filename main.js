370018066a4512282bbb2d52e3efb155ba48d80a
janinehtpps <116722944+janinehtpps@users.noreply.github.com>
2023-11-16 08:56:29 -0300

    Atualizar o main.js

--- main.js
+++ main.js
@@ -29,19 +29,5 @@ console.log(contador);
 }
 
 
-function tocaSom(idElementoAudio){
-    document.querySelector(idElementoAudio).play();
-}
 
-const listaDeTeclas = document.querySelectorAll(".tecla");
-
-let contador = 0;
-
-while(contador < listaDeTeclas.length){
-    const efeito = listaDeTeclas[contador].classList[1];
-    const idAudio = "#som_"+efeito;
-    listaDeTeclas[contador].onclick = function (){
-        tocaSom(idAudio);
-    }
-    contador = contador + 1;  
-}
+    
